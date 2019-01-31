8256ef9bbc1e2adffcf4faea203abeaa970af3e6
Umran Hussein <umran.hussein@gmail.com>
2019-01-31 06:31:50 -0800

    fixed indentation

--- src/generateGraphqlTypes.js
+++ src/generateGraphqlTypes.js
@@ -2,135 +2,133 @@ const { GraphQLObjectType, GraphQLList, GraphQLID, GraphQLInt, GraphQLFloat, Gra
 const { GraphQLDateTime } = require('graphql-iso-date')
 
 module.exports = (schemas, resolver) => {
-	const types = Object.keys(schemas).reduce((accumulator, schemaKey) => {
- 		accumulator[schemaKey] = {
-  		objectType: () => createObjectType(schemaKey, schemas, types, resolver)
-  	}
+  const types = Object.keys(schemas).reduce((accumulator, schemaKey) => {
+    accumulator[schemaKey] = {
+      objectType: () => createObjectType(schemaKey, schemas, types, resolver)
+    }
 
-  	return accumulator
+    return accumulator
     
- 	}, {})
+  }, {})
 
-	return types
+  return types
 }
 
 const createObjectType = (schemaKey, schemas, types, resolver) => {
-	const fields = schemas[schemaKey].fields
+  const fields = schemas[schemaKey].fields
 
-	return new GraphQLObjectType({
-		name: schemaKey,
-		fields: () => Object.keys(fields).reduce((accumulator, fieldKey) => {
-			accumulator[fieldKey] = generateField(fieldKey, fields[fieldKey], schemas, types, resolver)
-			
-			return accumulator
-		
-		}, { _id: { type: GraphQLID } })
-	})
+  return new GraphQLObjectType({
+    name: schemaKey,
+    fields: () => Object.keys(fields).reduce((accumulator, fieldKey) => {
+      accumulator[fieldKey] = generateField(fieldKey, fields[fieldKey], schemas, types, resolver)
+      
+      return accumulator
+    
+    }, { _id: { type: GraphQLID } })
+  })
 }
 
 const generateField = (fieldKey, field, schemas, types, resolver, inArray=false) => {
-	switch(field.type) {
-		case 'string':
-			return generateStringField(field, inArray)
-		case 'integer':
-			return generateIntegerField(field, inArray)
-		case 'float':
-			return generateFloatField(field, inArray)
-		case 'boolean':
-			return generateBooleanField(field, inArray)
-		case 'date':
-			return generateDateField(field, inArray)
-		case 'reference':
-			return generateReferenceField(fieldKey, field, schemas, types, resolver, inArray)
-		case 'association':
-			return generateReferenceField(fieldKey, field, schemas, types, resolver, inArray)
-		case 'array':
-			return generateArrayField(fieldKey, field, schemas, types, resolver)
-	}
+  switch(field.type) {
+    case 'string':
+      return generateStringField(field, inArray)
+    case 'integer':
+      return generateIntegerField(field, inArray)
+    case 'float':
+      return generateFloatField(field, inArray)
+    case 'boolean':
+      return generateBooleanField(field, inArray)
+    case 'date':
+      return generateDateField(field, inArray)
+    case 'reference':
+      return generateReferenceField(fieldKey, field, schemas, types, resolver, inArray)
+    case 'association':
+      return generateReferenceField(fieldKey, field, schemas, types, resolver, inArray)
+    case 'array':
+      return generateArrayField(fieldKey, field, schemas, types, resolver)
+  }
 }
 
 const generateStringField = (field, inArray) => {
-	if (inArray) {
-		return GraphQLString
-	}
+  if (inArray) {
+    return GraphQLString
+  }
 
-	return {
-		type: GraphQLString
-	}
+  return {
+    type: GraphQLString
+  }
 }
 
 const generateIntegerField = (field, inArray) => {
-	if (inArray) {
-		return GraphQLInt
-	}
+  if (inArray) {
+    return GraphQLInt
+  }
 
-	return {
-		type: GraphQLInt
-	}
+  return {
+    type: GraphQLInt
+  }
 }
 
 const generateFloatField = (field, inArray) => {
-	if (inArray) {
-		return GraphQLFloat
-	}
+  if (inArray) {
+    return GraphQLFloat
+  }
 
-	return {
-		type: GraphQLFloat
-	}
+  return {
+    type: GraphQLFloat
+  }
 }
 
 const generateBooleanField = (field, inArray) => {
-	if (inArray) {
-		return GraphQLBoolean
-	}
+  if (inArray) {
+    return GraphQLBoolean
+  }
 
-	return {
-		type: GraphQLBoolean
-	}
+  return {
+    type: GraphQLBoolean
+  }
 }
 
 const generateDateField = (field, inArray) => {
-	if (inArray) {
-		return GraphQLDateTime
-	}
+  if (inArray) {
+    return GraphQLDateTime
+  }
 
-	return {
-		type: GraphQLDateTime
-	}
+  return {
+    type: GraphQLDateTime
+  }
 }
 
 const generateReferenceField = (fieldKey, field, schemas, types, resolver, inArray) => {
 
-	if (inArray) {
-		return types[field.ref].objectType()
-	}
+  if (inArray) {
+    return types[field.ref].objectType()
+  }
 
-	let result = {
-		type: types[field.ref].objectType()
-	}
+  let result = {
+    type: types[field.ref].objectType()
+  }
 
-	if (schemas[field.ref] === 'collection') {
-		result.resolve = async (root, args, context) => {
-			args = { ...args, _id: root[fieldKey] }
-			return await resolver('findOne', field.ref, root, args, context)
-		}
-	}
+  if (schemas[field.ref] === 'collection') {
+    result.resolve = async (root, args, context) => {
+      args = { ...args, _id: root[fieldKey] }
+      return await resolver('findOne', field.ref, root, args, context)
+    }
+  }
 
-	return result
+  return result
 }
 
 const generateArrayField = (fieldKey, field, schemas, types, resolver) => {
-	let result = {
-		type: new GraphQLList(generateField(fieldKey, field.item, schemas, types, resolver, true))
-	}
+  let result = {
+    type: new GraphQLList(generateField(fieldKey, field.item, schemas, types, resolver, true))
+  }
 
-	if ((field.item.type === 'reference' || field.item.type === 'association') && schemas[field.item.ref] === 'collection') {
-		result.resolve = async (root, args, context) => {
-			args = { ...args, _id: { $in: root[fieldKey] } }
-			return await resolver('find', field.ref, root, args, context)
-		}
-	}
+  if ((field.item.type === 'reference' || field.item.type === 'association') && schemas[field.item.ref] === 'collection') {
+    result.resolve = async (root, args, context) => {
+      args = { ...args, _id: { $in: root[fieldKey] } }
+      return await resolver('find', field.ref, root, args, context)
+    }
+  }
 }
-
-
 
